4c23920df6c825c03e7640114dc2c74fffe663de
AmazingAbhishek <amazingabhishek@AmazingAbhisheks-MacBook-Air.local>
2021-01-02 12:58:40 +0530

    progress bar added

--- webview-master-splashscreen/app/src/main/java/com/tobtek/shop/MainActivity.java
+++ webview-master-splashscreen/app/src/main/java/com/tobtek/shop/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.FrameLayout;
+import android.widget.ProgressBar;
 
 public class MainActivity extends Activity {
 
@@ -17,13 +19,14 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mWebView = findViewById(R.id.activity_main_webview);
+
         WebSettings webSettings = mWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
-        mWebView.setWebViewClient(new MyWebViewClient());
+        mWebView.setWebViewClient(new MyWebViewClient(this, (FrameLayout) findViewById(R.id.loader)));
 
         // REMOTE RESOURCE
         mWebView.loadUrl("https://exoti.cyou/");
-        mWebView.setWebViewClient(new WebViewClient());
+//        mWebView.setWebViewClient(new WebViewClient());
 
         // LOCAL RESOURCE
         // mWebView.loadUrl("file:///android_asset/index.html");
--- webview-master-splashscreen/app/src/main/java/com/tobtek/shop/MyWebViewClient.java
+++ webview-master-splashscreen/app/src/main/java/com/tobtek/shop/MyWebViewClient.java
@@ -1,25 +1,70 @@
 package com.tobtek.shop;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.FrameLayout;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 class MyWebViewClient extends WebViewClient {
 
+    private Activity activity;
+    private FrameLayout progressBar;
+
+    public MyWebViewClient(Activity activity, FrameLayout progressBar) {
+        this.activity = activity;
+        this.progressBar = progressBar;
+    }
+
+    @Override
+    public void onPageStarted(WebView view, String url, Bitmap favicon) {
+        // TODO Auto-generated method stub
+        super.onPageStarted(view, url, favicon);
+//        Toast.makeText(activity, "Started", Toast.LENGTH_SHORT).show();
+    }
+
     @Override
     public boolean shouldOverrideUrlLoading(WebView view, String url) {
-        String hostname;
-
-        // YOUR HOSTNAME
-        hostname = "https://exoti.cyou/";
-
-        Uri uri = Uri.parse(url);
-        if (url.startsWith("file:") || uri.getHost() != null && uri.getHost().endsWith(hostname)) {
-            return false;
-        }
-        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-        view.getContext().startActivity(intent);
+        view.loadUrl(url);
         return true;
+    }
+
+    @Override
+    public void onPageFinished(WebView view, String url) {
+        //Page load finished
+        progressBar.setVisibility(View.GONE);
+        super.onPageFinished(view, url);
+//        Toast.makeText(activity, "finished", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+        // TODO Auto-generated method stub
+        super.onReceivedError(view, errorCode, description, failingUrl);
+        progressBar.setVisibility(View.GONE);
+
+        new AlertDialog.Builder(activity)
+                .setTitle("Error :(")
+                .setMessage("Something went wrong please restart the app or check the internet connection")
+
+                // Specifying a listener allows you to take an action before dismissing the dialog.
+                // The dialog is automatically dismissed when a dialog button is clicked.
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        activity.finish();
+                    }
+                })
+
+                // A null listener allows the button to dismiss the dialog and take no further action.
+//                .setNegativeButton(android.R.string.no, null)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
     }
 }
